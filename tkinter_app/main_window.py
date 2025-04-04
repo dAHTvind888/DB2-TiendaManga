c91b8cd12db029654fd81e3c0a705a49facb2262
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2025-04-04 13:46:59 -0400

    Interfaz-Actualizacion 1

---
+++ tkinter_app/main_window.py
@@ -0,0 +1,54 @@
+from SQLServer_functions import * 
+
+# Main window
+def start_window():
+    #Tk() crea la ventana
+    main_window = tk.Tk()
+    #geomtry() is el tamano de la venta
+    main_window.geometry("400x300")
+    #.title() is el titulo de la ventana
+    main_window.title("Log In")
+    
+    labelUsuario = tk.Label(main_window, text="Usuario: ")
+    labelUsuario.pack(pady=5)
+    entryUsuario = tk.Entry(main_window)
+    entryUsuario.pack(pady=5)
+
+    labelPassword = tk.Label(main_window, text="Password: ")
+    labelPassword.pack(pady=5)
+    entryPassword = tk.Entry(main_window)
+    entryPassword.pack(pady=5)
+
+    botonConectar = tk.Button(main_window, 
+                          text="Conectar", 
+                          command=lambda: conectar(entryUsuario.get(), entryPassword.get())
+                          )
+    botonConectar.pack(pady=10)
+    '''
+    #.Button(window, text, command)
+    #window is la venta en donde se cread el Button
+    #text is el texto del button
+    #command is la funcion que se llama al clickar el button
+    add_button = tk.Button(main_window, text="Add Data", command=add_select_table)
+    #.pack() se usa siempre que se anade algo a una venta
+    #pad y/x is la distancia entre los objetos
+    add_button.pack(pady=10)
+
+    visualize_button = tk.Button(main_window, text="Visualize Data", command=visualize_select_table)
+    visualize_button.pack(pady=10)
+
+    delete_button = tk.Button(main_window, text="Delete Data", command=delete_select_table)
+    delete_button.pack(pady=10)
+
+    modify_button = tk.Button(main_window, text="Modify Data", command=modify_select_table)
+    modify_button.pack(pady=10)
+    #mainloop() is lo que mantiene 'alerta' a las acciones del usuario
+    '''
+    main_window.mainloop()
+
+start_window()
+
+
+
+    
+
